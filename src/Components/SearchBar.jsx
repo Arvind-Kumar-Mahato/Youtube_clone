84fb956953f14babd4a64fba315544c4d507f2ea
Arvind-Kumar-Mahato <93374171+Arvind-Kumar-Mahato@users.noreply.github.com>
2022-09-23 20:37:38 +0530

    search videos are working fine

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -4,10 +4,24 @@ import { useNavigate } from 'react-router-dom';
 import {Paper, IconButton} from "@mui/material";
 import {Search} from '@mui/icons-material';
 const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+   const navigate = useNavigate();
+   
+
+
+   const onhandleSubmit = (e) => {
+     e.preventDefault();
+
+     if (searchTerm) {
+       navigate(`/search/${searchTerm}`);
+
+       setSearchTerm("");
+     }
+   };
   return (
     <Paper
       component="form"
-      onSubmit={()=>{}}
+      onSubmit={onhandleSubmit}
       sx={{
         borderRadius: 20,
         border: "1px solid #e3e3e3",
@@ -16,13 +30,14 @@ const SearchBar = () => {
         mr: { sm: 5 },
       }}
     >
-      <input className='search-bar'
-        placeholder='Search...'
-        value = ""
-        onChange = {()=>{}}
+      <input
+        className="search-bar"
+        placeholder="Search..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
       />
-      <IconButton type = "submit" sx= {{p: '10px',color:'red'}}>
-        <Search/>
+      <IconButton type="submit" sx={{ p: "10px", color: "red" }}>
+        <Search />
       </IconButton>
     </Paper>
   );
